9e5d05f19227a1e5c43ea9db0e710ac2613cbe85
cikhub <hesenisgenderli999@gmail.com>
2020-10-31 17:52:33 +0400

    modified setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3"
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3",
         "Topic :: Database",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],  
